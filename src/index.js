d5a17c3b2b5553f7c4d2c7894078b41e474265be
Denys Tkachenko <sinedtex@gmail.com>
2025-08-04 19:53:43 +0300

    comp stage 1 + stage 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
 
-const bootstrap = async () => {
-    await initMongoConnection();
-    setupServer();
-};
-
+await initMongoConnection();
 bootstrap();
